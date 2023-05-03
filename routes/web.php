7105ff1732f5585f6d99815541b3370f34e9bd28
Donald Nwokoro <infect3dlab@gmail.com>
2023-05-03 12:33:15 +0100

    Introduction to Views: Looping over arrays to views I

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
     return view('job', [
         'heading' => 'Latest Jobs',
         'jobs' => [
+        [
             'id' => 1,
             'title' => 'Senior Python Developer',
             'description' => 'XBorg is revolutionizing the gaming industry by empowering gamers to create their gaming digital identity through a credential network that paves the way for a new generation of enhanced gaming applications, such as more efficient matchmaking, reputation-based lending and fan engagement apps. With the potential to onboard tens of millions of players into the Web3 ecosystem, XBorg is set to transform the future of gaming as we know it with its gaming protocol. XBorg is also home to the most competitive Web3 gaming community that actively helps us shape the best products. '
@@ -31,6 +32,7 @@ Route::get('/', function () {
             'id' => 3,
             'title' => 'Senior Typescript Developer',
             'description' => 'XBorg is revolutionizing the gaming industry by empowering gamers to create their gaming digital identity through a credential network that paves the way for a new generation of enhanced gaming applications, such as more efficient matchmaking, reputation-based lending and fan engagement apps. With the potential to onboard tens of millions of players into the Web3 ecosystem, XBorg is set to transform the future of gaming as we know it with its gaming protocol. XBorg is also home to the most competitive Web3 gaming community that actively helps us shape the best products. '
+        ]
         ]
     ]); 
 });
