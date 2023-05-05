db13f99ed04603253770d8017fde05b49df9d6cd
Donald Nwokoro <infect3dlab@gmail.com>
2023-05-05 11:04:43 +0100

    Introduction to Blade Directives I

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -1,7 +1,12 @@
 <h1>{{ $heading }}</h1>
 
+@php
+    $hello = "Donald"
+@endphp
+
 {{-- Directives --}}
 @foreach($jobs as $job)
     <h2>{{ $job['title'] }}</h2>
     <p>{{ $job['description'] }}</p>
+    <b>{{ $hello }}</b>
 @endforeach
