a7045d8c411125f99ecd3f9c925d78e40d90fc8d
Donald Nwokoro <infect3dlab@gmail.com>
2023-05-05 11:33:37 +0100

    Introduction to Models

---
+++ app/Models/Jobs.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+
+class Jobs{
+    public static function all(){
+        return [
+            [
+                'id' => 1,
+                'title' => 'Senior Python Developer',
+                'description' => 'XBorg is revolutionizing the gaming industry by empowering gamers to create their gaming digital identity through a credential network that paves the way for a new generation of enhanced gaming applications, such as more efficient matchmaking, reputation-based lending and fan engagement apps. With the potential to onboard tens of millions of players into the Web3 ecosystem, XBorg is set to transform the future of gaming as we know it with its gaming protocol. XBorg is also home to the most competitive Web3 gaming community that actively helps us shape the best products. '
+            ],
+            [
+                'id' => 2,
+                'title' => 'Junior Laravel Developer',
+                'description' => 'XBorg is revolutionizing the gaming industry by empowering gamers to create their gaming digital identity through a credential network that paves the way for a new generation of enhanced gaming applications, such as more efficient matchmaking, reputation-based lending and fan engagement apps. With the potential to onboard tens of millions of players into the Web3 ecosystem, XBorg is set to transform the future of gaming as we know it with its gaming protocol. XBorg is also home to the most competitive Web3 gaming community that actively helps us shape the best products. '
+            ],
+            [
+                'id' => 3,
+                'title' => 'Senior Typescript Developer',
+                'description' => 'XBorg is revolutionizing the gaming industry by empowering gamers to create their gaming digital identity through a credential network that paves the way for a new generation of enhanced gaming applications, such as more efficient matchmaking, reputation-based lending and fan engagement apps. With the potential to onboard tens of millions of players into the Web3 ecosystem, XBorg is set to transform the future of gaming as we know it with its gaming protocol. XBorg is also home to the most competitive Web3 gaming community that actively helps us shape the best products. '
+            ]
+            ];
+    }
+}
--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -1,10 +1,16 @@
 <h1>{{ $heading }}</h1>
 
+{{-- Directives --}}
+
 @php
     $hello = "Donald"
 @endphp
 
-{{-- Directives --}}
+@if (count($jobs) == 0)
+    <p style="color: red">No Jobs Available</p>
+@endif
+
+
 @foreach($jobs as $job)
     <h2>{{ $job['title'] }}</h2>
     <p>{{ $job['description'] }}</p>
