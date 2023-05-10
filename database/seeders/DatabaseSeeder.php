428e455bb083250caf717c70dc60a636bb09e077
Donald Nwokoro <infect3dlab@gmail.com>
2023-05-10 13:04:39 +0100

    Created Factory for Jobs I

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,24 +16,26 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(100)->create();
 
-        Jobs::create([
-            'title' => 'Laravel Senior Developer', 
-            'tags' => 'laravel, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'email1@email.com',
-            'website' => 'https://www.acme.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-        ],);
+        Jobs::factory(150)->create();
 
-        Jobs::create([
-            'title' => 'React Senior Developer', 
-            'tags' => 'react, javascript',
-            'company' => 'Amazon Corp',
-            'location' => 'Krig, SA',
-            'email' => 'email2@email.com',
-            'website' => 'https://www.amazon.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-        ],); 
+        // Jobs::create([
+        //     'title' => 'Laravel Senior Developer', 
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        // ],);
+
+        // Jobs::create([
+        //     'title' => 'React Senior Developer', 
+        //     'tags' => 'react, javascript',
+        //     'company' => 'Amazon Corp',
+        //     'location' => 'Krig, SA',
+        //     'email' => 'email2@email.com',
+        //     'website' => 'https://www.amazon.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        // ],); 
     }
 }
