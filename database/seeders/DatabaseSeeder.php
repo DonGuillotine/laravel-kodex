230eaa4ad5de9ace3a5e94bc53f469a8b7eacdd8
Donald Nwokoro <infect3dlab@gmail.com>
2023-05-11 11:52:26 +0100

    Cleaned up DatabaseSeeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,25 +17,5 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(100)->create();
 
         Jobs::factory(150)->create();
-
-        // Jobs::create([
-        //     'title' => 'Laravel Senior Developer', 
-        //     'tags' => 'laravel, javascript',
-        //     'company' => 'Acme Corp',
-        //     'location' => 'Boston, MA',
-        //     'email' => 'email1@email.com',
-        //     'website' => 'https://www.acme.com',
-        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-        // ],);
-
-        // Jobs::create([
-        //     'title' => 'React Senior Developer', 
-        //     'tags' => 'react, javascript',
-        //     'company' => 'Amazon Corp',
-        //     'location' => 'Krig, SA',
-        //     'email' => 'email2@email.com',
-        //     'website' => 'https://www.amazon.com',
-        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-        // ],); 
     }
 }
