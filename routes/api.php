744d27ad959a515acfe07ec7b75b070b29f7658d
Donald Nwokoro <infect3dlab@gmail.com>
2023-05-12 16:49:24 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/posts', function(){
     return response()->json([
-        'posts' => [
-            'title' => 'Post 1'
-        ]
+        
         ]);
 });
 
