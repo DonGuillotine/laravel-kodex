8915778aee6cf0dd7205a254b0eb2ea2029c37d8
Donald Nwokoro <infect3dlab@gmail.com>
2023-05-24 11:57:08 +0100

    Introduction to Props

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,7 +1,9 @@
+@props(['job'])
+
 <div class="col-lg-4 col-md-6 col-12 mt--30" data-sal="slide-up" data-sal-duration="700">
     <div class="rn-card box-card-style-default">
         <div class="inner">
-            <div class="thumbnail"><a class="image" href="/single_job/{{ $job['id'] }}"><img class="w-100" src="{{ asset('images/blog-grid/blog-01.jpg') }}" alt="Blog Image"></a>
+            <div class="thumbnail"><a class="image" href="/single_job/{{ $job->id }}"><img class="w-100" src="{{ asset('images/blog-grid/blog-01.jpg') }}" alt="Blog Image"></a>
             </div>
             <div class="content">
                 <ul class="rn-meta-list">
