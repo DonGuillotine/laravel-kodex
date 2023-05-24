00fcd9cf545ac955835ca5d6b2eb91b390c55f64
Donald Nwokoro <infect3dlab@gmail.com>
2023-05-24 12:53:58 +0100

    Added tag filter link

--- resources/views/components/job-tags.blade.php
+++ resources/views/components/job-tags.blade.php
@@ -7,7 +7,7 @@
 <div class="inner mt--20">
     <div class="tagcloud">
         @foreach ($tags as $tag)
-            <a href="#">Corporate</a>
+            <a href="/?tag={{ $tag }}">{{ $tag }}</a>
         @endforeach
     </div>
 </div>
