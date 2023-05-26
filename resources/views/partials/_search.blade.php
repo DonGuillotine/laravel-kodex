27ede41724cecb29840034968cc05229060e5eb8
Donald Nwokoro <infect3dlab@gmail.com>
2023-05-26 12:31:29 +0100

    Added Search Form action and input name

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -2,7 +2,8 @@
     <div class="col-lg-6 justify-content-center">
         <div class="rbt-single-widget widget_search mt--40">
             <div class="inner">
-                <form class="blog-search" action="#"><input type="text" placeholder="Search ...">
+                <form class="blog-search" action="/">
+                    <input name="search" type="text" placeholder="Search ...">
                     <button class="search-button">
                         <i class="feather-search"></i>
                     </button>
