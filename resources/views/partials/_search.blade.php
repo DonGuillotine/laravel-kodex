3a2d285c600b9ab64976663fe7791680c5d4683e
Donald Nwokoro <infect3dlab@gmail.com>
2023-05-26 12:39:01 +0100

    Added Search to scopeFilter function

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,10 +1,10 @@
 <div class="container">
-    <div class="col-lg-6 justify-content-center">
+    <div class="col-lg-12 justify-content-center mt--100">
         <div class="rbt-single-widget widget_search mt--40">
             <div class="inner">
                 <form class="blog-search" action="/">
                     <input name="search" type="text" placeholder="Search ...">
-                    <button class="search-button">
+                    <button type="submit" class="search-button">
                         <i class="feather-search"></i>
                     </button>
                 </form>
