51f6616a53d2c13c0c2c2545ef652cd5caeefe1d
Donald Nwokoro <infect3dlab@gmail.com>
2023-06-07 13:31:22 +0100

    Registeration Store Controller Created

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     // Show Registeration Form
     public function create(){
         return view('users.register');
+    }
+
+    // Store Registered Users
+    public function store(Request $request){
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required, confirmed, min:6']
+        ]);
     }
 }
