eb16e0c22cd3c2f6b0371ab03ebdbcdb5aafe047
Donald Nwokoro <infect3dlab@gmail.com>
2023-06-09 12:14:27 +0100

    Database relationships I

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Jobs;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,8 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(100)->create();
+        // \App\Models\User::factory(100)->create();
 
-        Jobs::factory(100)->create();
+        $user = User::factory()->create([
+            'name' => 'Pagan Min',
+            'email' => 'paganmin@farcry.com'
+        ]);
+
+        Jobs::factory(6)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
